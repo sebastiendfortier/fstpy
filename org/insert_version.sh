24c2f46951cdeb9eadae6972e5a7829913bf2bad
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-03-14 22:27:42 +0000

    wip

---
+++ org/insert_version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+#echo ${DIR}
+ROOT_DIR=${DIR:0:${#DIR}-3}
+#echo ${ROOT_DIR}
+cd ${DIR}
+VERSION=$(head -n 1 ${ROOT_DIR}VERSION)
+#echo ${VERSION}
+sed -i 's/_VERSION_/${VERSION}/g' install.org
+sed -i 's/_VERSION_/${VERSION}/g' usage.org
