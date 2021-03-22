b7766b4794fb336fa47194894f7b2aaaf48d9949
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-03-22 15:05:56 +0000

    wip

--- org/other/make_readme.sh
+++ org/other/make_readme.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+. ssmuse-sh -x cmd/cmdm/satellite/master_u1/miniconda3_4.9.2_ubuntu-18.04-skylake-64
+. activate fstpy_full
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROOT_DIR=${DIR:0:${#DIR}-9}
 cd ${DIR}
 #echo ${ROOT_DIR}
-/home/sbf000/.conda/envs/fstpy_dev/bin/pandoc -f org -t gfm README.org -o ${ROOT_DIR}README.md
+pandoc -f org -t gfm README.org -o ${ROOT_DIR}README.md
