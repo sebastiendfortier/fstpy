fcbcff5e036ce7d7ecc3af5eeb8e693ed8587485
Oleksandr Huziy <oleksandr.huziy@canada.ca>
2021-08-03 17:49:59 +0000

    Tests: Allow users to redefine HOST_NUM
    to be able to run tests from hpcr-vis or other.. default is 3

--- test/__init__.py
+++ test/__init__.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 import os
 
-HOST_NUM = os.getenv("TRUE_HOST")[-1]
+DEFAULT_HOST_NUM = 3
+HOST_NUM = os.getenv("FSTPY_HOST_NUM", None)
+
+if HOST_NUM is not None:
+    HOST_NUM = int(DEFAULT_HOST_NUM)
+else:
+    HOST_NUM = os.getenv("TRUE_HOST")[-1]
+    try:
+        HOST_NUM = int(HOST_NUM)
+    except ValueError as ve:
+        HOST_NUM = DEFAULT_HOST_NUM
+
 USER = os.getenv("USER")
 SPOOKI_TMPDIR = os.getenv("SPOOKI_TMPDIR")
 TMPDIR = os.getenv("TMPDIR")
-TEST_PATH = "/fs/site%s/eccc/cmd/w/spst900/spooki/spooki_dir/pluginsRelatedStuff/"%HOST_NUM
+TEST_PATH = "/fs/site%d/eccc/cmd/w/spst900/spooki/spooki_dir/pluginsRelatedStuff/" % HOST_NUM
 TMP_PATH = SPOOKI_TMPDIR if SPOOKI_TMPDIR else TMPDIR
