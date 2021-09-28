f882dc2e95d51abd6f95447c4fbe8566bdc573d1
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-09-28 12:19:48 +0000

    add log to all interface

--- fstpy/all.py
+++ fstpy/all.py
@@ -1,7 +1,4 @@
-from . import (LEVELTYPES, STATIONSFB, STDVAR, THERMO_CONSTANTS, UNITS,
-                   VCTYPES)
-from .std_io import get_2d_lat_lon
-
+from . import LEVELTYPES, STATIONSFB, STDVAR, THERMO_CONSTANTS, UNITS, VCTYPES
 from .dataframe import (add_columns, add_data_type_str_column,
                         add_decoded_date_column, add_forecast_hour_column,
                         add_ip_info_columns, add_parsed_etiket_columns,
@@ -9,9 +6,11 @@ from .dataframe import (add_columns, add_data_type_str_column,
                         get_meta_fields_exists)
 from .dataframe_utils import fststat, metadata_cleanup, select_with_meta, voir
 from .log import setup_fstpy_logger
-from .std_io import get_basic_dataframe, get_dataframe_from_file
+from .std_io import (get_2d_lat_lon, get_basic_dataframe,
+                     get_dataframe_from_file)
 from .std_reader import StandardFileReader, compute, to_cmc_xarray
 from .std_writer import StandardFileWriter
 from .unit import get_converter, unit_convert, unit_convert_array
-from .utils import column_descriptions, delete_file, get_num_rows_for_reading
+from .utils import (column_descriptions, delete_file, get_num_rows_for_reading,
+                    to_dask, to_numpy)
 from .xarray_utils import comvert_to_cmc_xarray
