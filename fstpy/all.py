8b855bd06bf81858c392c41a57187f957712e8a4
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-10-08 10:57:54 -0400

    add add_shape_column to interface

--- fstpy/all.py
+++ fstpy/all.py
@@ -3,7 +3,7 @@ from .dataframe import (add_columns, add_data_type_str_column,
                         add_decoded_date_column, add_forecast_hour_column,
                         add_ip_info_columns, add_parsed_etiket_columns,
                         add_unit_and_description_columns,
-                        get_meta_fields_exists)
+                        get_meta_fields_exists,add_shape_column)
 from .dataframe_utils import fststat, metadata_cleanup, select_with_meta, voir
 from .log import setup_fstpy_logger
 from .std_io import (get_2d_lat_lon, get_basic_dataframe,
