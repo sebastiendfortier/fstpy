d51ec5f6c1894864fbb4961d2edd603da25b1acf
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-11-17 12:14:26 +0000

    give access to all functions from all

--- fstpy/all.py
+++ fstpy/all.py
@@ -1,16 +1,12 @@
-from . import LEVELTYPES, STATIONSFB, STDVAR, UNITS, VCTYPES
-from .dataframe import (add_columns, add_data_type_str_column,
-                        add_decoded_date_column, add_forecast_hour_column,
-                        add_ip_info_columns, add_parsed_etiket_columns,
-                        add_unit_and_description_columns,
-                        get_meta_fields_exists,add_shape_column)
-from .dataframe_utils import fststat, metadata_cleanup, select_with_meta, voir
-from .log import setup_fstpy_logger
-from .std_io import (get_2d_lat_lon, get_basic_dataframe,
-                     get_dataframe_from_file)
-from .std_reader import StandardFileReader, compute, to_cmc_xarray
-from .std_writer import StandardFileWriter
-from .unit import get_converter, unit_convert, unit_convert_array
-from .utils import (column_descriptions, delete_file, get_num_rows_for_reading,
-                    to_dask, to_numpy, FstPrecision)
-from .xarray_utils import convert_to_cmc_xarray
+from . import *
+from .dataframe import *
+from .dataframe_utils import *
+from .log import *
+from .std_dec import *
+from .std_enc import *
+from .std_io import *
+from .std_reader import *
+from .std_writer import *
+from .unit import *
+from .utils import *
+from .xarray_utils import *
