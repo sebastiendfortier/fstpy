d15c46342749e0932989d3a88a8a67a5ec8bbbd4
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-11-17 12:14:05 +0000

    add possibility to view code in doc

--- doc/conf.py
+++ doc/conf.py
@@ -39,7 +39,8 @@ extensions = ['sphinx.ext.napoleon',
               'sphinx.ext.doctest', 
               'sphinx_autodoc_typehints',
               'sphinx_gallery.gen_gallery',
-              'nbsphinx']
+              'nbsphinx',
+              'sphinx.ext.viewcode']
 
 napoleon_include_private_with_doc = False
 
