3c7f72656ecbc9d5bfaa6b4a2372e19abb5af0f2
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-11-22 14:53:23 +0000

    add std_grid tools

--- fstpy/all.py
+++ fstpy/all.py
@@ -4,9 +4,11 @@ from .dataframe_utils import *
 from .log import *
 from .std_dec import *
 from .std_enc import *
+from .std_grid import *
 from .std_io import *
 from .std_reader import *
 from .std_writer import *
 from .unit import *
 from .utils import *
 from .xarray_utils import *
+from .std_grid import *
