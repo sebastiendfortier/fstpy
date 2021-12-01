7a3d5001129d7d7c7eee582c53722cc0cf9d75d9
Sébastien Fortier <sebastien.fortier@canada.ca>
2021-12-01 14:15:58 +0000

    refactoring

--- fstpy/std_grid.py
+++ fstpy/std_grid.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 import rpnpy.vgd.all as vgd
 
-def get_df_from_grid(grid_params:dict) -> pd.DataFrame:
+
+def get_df_from_grid(grid_params: dict) -> pd.DataFrame:
     """For grid types Z, Y or U, produces a DataFrame of >>,^^ or ^> fields
 
     :param grid_params: horizontal grid definition dictionnary
@@ -13,12 +14,27 @@ def get_df_from_grid(grid_params:dict) -> pd.DataFrame:
     """
     g = grid_params
     if (g['grtyp'] == 'Z') or (g['grtyp'] == 'Y'):
-        meta_df = pd.DataFrame([{'nomvar':'>>', 'typvar':'X', 'etiket':'', 'ni':g['ni'], 'nj':1, 'nk':1, 'dateo':0, 'ip1':g['ig1'], 'ip2':g['ig2'], 'ip3':0, 'deet':0, 'npas':0, 'datyp':5, 'nbits':32, 'grtyp':g['grref'], 'ig1':g['ig1ref'], 'ig2':g['ig2ref'], 'ig3':g['ig3ref'], 'ig4':g['ig4ref'], 'datev':0, 'd':g['ax']},
-                            {'nomvar':'^^', 'typvar':'X', 'etiket':'', 'ni':1, 'nj':g['nj'], 'nk':1, 'dateo':0, 'ip1':g['ig1'], 'ip2':g['ig2'], 'ip3':0, 'deet':0, 'npas':0, 'datyp':5, 'nbits':32, 'grtyp':g['grref'], 'ig1':g['ig1ref'], 'ig2':g['ig2ref'], 'ig3':g['ig3ref'], 'ig4':g['ig4ref'], 'datev':0, 'd':g['ay']} ])
+        meta_df = pd.DataFrame(
+            [
+                {'nomvar': '>>', 'typvar': 'X', 'etiket': '', 'ni': g['ni'], 'nj':1, 'nk':1, 'dateo':0, 'ip1':g['ig1'], 
+                'ip2':g['ig2'], 'ip3':0, 'deet':0, 'npas':0, 'datyp':5, 'nbits':32, 'grtyp':g['grref'], 
+                'ig1':g['ig1ref'], 'ig2':g['ig2ref'], 'ig3':g['ig3ref'], 'ig4':g['ig4ref'], 'datev':0, 'd':g['ax']},
+                {'nomvar': '^^', 'typvar': 'X', 'etiket': '', 'ni': 1, 'nj': g['nj'], 'nk':1, 'dateo':0, 'ip1':g['ig1'], 
+                'ip2':g['ig2'], 'ip3':0, 'deet':0, 'npas':0, 'datyp':5, 'nbits':32, 'grtyp':g['grref'], 
+                'ig1':g['ig1ref'], 'ig2':g['ig2ref'], 'ig3':g['ig3ref'], 'ig4':g['ig4ref'], 'datev':0, 'd':g['ay']}
+            ]
+            )
+
     elif g['grtyp'] == 'U':
-        meta_df = pd.DataFrame([{'nomvar':'^>', 'typvar':'X', 'etiket':'', 'ni':g['axy'].shape[0], 'nj':1, 'nk':1, 'dateo':0, 'ip1':g['ig1'], 'ip2':g['ig2'], 'ip3':0, 'deet':0, 'npas':0, 'datyp':5, 'nbits':32, 'grtyp':g['grref'], 'ig1':g['ig1ref'], 'ig2':g['ig2ref'], 'ig3':g['ig3ref'], 'ig4':g['ig4ref'], 'datev':0, 'd':g['axy']}])
+        meta_df = pd.DataFrame(
+            [
+                {'nomvar': '^>', 'typvar': 'X', 'etiket': '', 'ni': g['axy'].shape[0], 'nj':1, 'nk':1, 'dateo':0, 
+                'ip1':g['ig1'], 'ip2':g['ig2'], 'ip3':0, 'deet':0, 'npas':0, 'datyp':5, 'nbits':32, 'grtyp':g['grref'], 
+                'ig1':g['ig1ref'], 'ig2':g['ig2ref'], 'ig3':g['ig3ref'], 'ig4':g['ig4ref'], 'datev':0, 'd':g['axy']}
+            ]
+            )
     else:
-        meta_df = pd.DataFrame(dtype=object)    
+        meta_df = pd.DataFrame(dtype=object)
     return meta_df
 
 # def get_df_from_vgrid(vgrid_descriptor: vgd.VGridDescriptor, ip1: int, ip2: int) -> pd.DataFrame:
@@ -54,37 +70,36 @@ def get_df_from_grid(grid_params:dict) -> pd.DataFrame:
 # }
 # g = rmn.encodeGrid(gp)
 
-# 'xlat1': 0.0, 
-# 'xlon1': 180.0, 
-# 'xlat2': 1.0, 
-# 'xlon2': 270.0, 
-# 'ni': 90, 
-# 'nj': 45, 
-# 'rlat0': 34.059606166461926, 
-# 'rlon0': 250.23401123256826, 
-# 'dlat': 0.5, 
-# 'dlon': 0.5, 
-# 'lat0': 35.0, 
-# 'lon0': 250.0, 
-# 'grtyp': 'Z', 
-# 'grref': 'E', 
+# 'xlat1': 0.0,
+# 'xlon1': 180.0,
+# 'xlat2': 1.0,
+# 'xlon2': 270.0,
+# 'ni': 90,
+# 'nj': 45,
+# 'rlat0': 34.059606166461926,
+# 'rlon0': 250.23401123256826,
+# 'dlat': 0.5,
+# 'dlon': 0.5,
+# 'lat0': 35.0,
+# 'lon0': 250.0,
+# 'grtyp': 'Z',
+# 'grref': 'E',
 # 'ig1ref': 900,          ig1
 # 'ig2ref': 10,           ig2
 # 'ig3ref': 43200,        ig3
 # 'ig4ref': 43200,        ig4
-# 'ig1': 66848, 
-# 'ig2': 39563, 
-# 'ig3': 0, 
-# 'ig4': 0, 
-# 'id': 0, 
-# 'tag1': 66848, 
-# 'tag2': 39563, 
-# 'tag3': 0, 
+# 'ig1': 66848,
+# 'ig2': 39563,
+# 'ig3': 0,
+# 'ig4': 0,
+# 'id': 0,
+# 'tag1': 66848,
+# 'tag2': 39563,
+# 'tag3': 0,
 # 'shape': (90, 45)}
-#   nomvar typvar etiket  ni  nj  nk  dateo    ip1    ip2  ip3  ...  datyp  nbits  grtyp  ig1 ig2    ig3    ig4  datev        grid  
-# 0     >>      X         90   1   1      0  66848  39563    0  ...      5     32      E  900  10  43200  43200      0  6684839563   
-# 1     ^^      X          1  45   1      0  66848  39563    0  ...      5     32      E  900  10  43200  43200      0  6684839563   
+#   nomvar typvar etiket  ni  nj  nk  dateo    ip1    ip2  ip3  ...  datyp  nbits  grtyp  ig1 ig2    ig3    ig4  datev        grid
+# 0     >>      X         90   1   1      0  66848  39563    0  ...      5     32      E  900  10  43200  43200      0  6684839563
+# 1     ^^      X          1  45   1      0  66848  39563    0  ...      5     32      E  900  10  43200  43200      0  6684839563
 
 # {'nomvar':'>>', typvar:'X', 'etiket':'', 'ni':g.ni, nj:1, 'nk':1, 'dateo':0, 'ip1':g.ig1, 'ip2':g.ig2, 'ip3':0, 'datyp':5, 'nbits':32, 'grtyp':g.grref, 'ig1':g.ig1ref, 'ig2':g.ig2ref, 'ig3':g.ig3ref, 'ig4'g.ig4ref:, 'datev':0, 'd':g.ax}
 # {'nomvar':'^^', typvar:'X', 'etiket':'', 'ni':1, nj:g.nj, 'nk':1, 'dateo':0, 'ip1':g.ig1, 'ip2':g.ig2, 'ip3':0, 'datyp':5, 'nbits':32, 'grtyp':g.grref, 'ig1':g.ig1ref, 'ig2':g.ig2ref, 'ig3':g.ig3ref, 'ig4'g.ig4ref:, 'datev':0, 'd':g.ay}
-
