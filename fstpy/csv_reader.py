ad2edd89ff0b1dacd8261fd91f6641bd4f777c22
zak000 <zakaria.haimeur@ec.gc.ca>
2022-02-01 18:56:58 +0000

    changement ds csv_reader et tutorial

--- fstpy/csv_reader.py
+++ fstpy/csv_reader.py
@@ -1,3 +1,45 @@
-import pandas as pd 
-df = pd.read_csv('/fs/site3/eccc/cmd/w/spst900/spooki/spooki_dir/pluginsRelatedStuff/ReaderCsv/testsFiles/gds1_pds1_level2.csv')
-df
+from mimetypes import init
+import pandas as pd
+import os
+from fstpy.utils import initializer 
+
+
+#BASE_COLUMNS = ['nomvar', 'typvar', 'etiket', 'ni', 'nj', 'nk', 'dateo', 'ip1', 'ip2', 'ip3', 'deet', 'npas','datyp', 'nbits', 'grtyp', 'ig1', 'ig2', 'ig3', 'ig4', 'datev', 'grid', 'd']
+#key – Positioning information to the record. Obtained with fstinf or fstinl.
+#dateo – date of origin (date time stamp) Cannot change dateo and datev.
+#datev – date of validity (date time stamp) Cannot change dateo and datev.
+#deet – Length of a time step in seconds datev constant unless keep_dateo
+#npas – time step number datev constant unless keep_dateo
+#ni – first dimension of the data field
+#nj – second dimension of the data field
+#nk – third dimension of the data field
+#nbits – number of bits kept for the elements of the field
+#datyp – data type of the elements
+#ip1 – vertical level
+#ip2 – forecast hour
+#ip3 – user defined identifier
+#typvar – type of field (forecast, analysis, climatology)
+#nomvar – variable name
+#etiket – label
+#grtyp – type of geographical projection
+#ig1 – first grid descriptor
+#ig2 – second grid descriptor
+#ig3 – third grid descriptor
+#ig4 – fourth grid descriptor/
+
+class CsvFileReaderError(Exception):
+    pass
+
+meta_data = ["^>", ">>", "^^", "!!", "!!SF", "HY", "P0", "PT", "E1"]
+
+class CsvFileReader :
+    @initializer
+    def __init__(self,filenames,decode_metadata = False, query = None):
+        if isinstance(self.filenames,list):
+            self.filenames = os.path.abspath(str(self.filenames))
+        elif isinstance(self.filenames, list):
+            self.filenames = [os.path.abspath(str(f)) for f in filenames]
+        else:
+            raise CsvFileReaderError('Filenames must be string or list\n')
+
+        pass
