af267497c5278f32ad34001369ed4c61a81cad9a
zak000 <zakaria.haimeur@ec.gc.ca>
2022-02-08 23:56:30 +0000

    writer functions

--- fstpy/csv_reader.py
+++ fstpy/csv_reader.py
@@ -10,6 +10,7 @@ import os
 import numpy as np
 from .utils import initializer
 import csv
+import datetime
 
 
 BASE_COLUMNS = ['nomvar','level','typvar', 'etiket', 'dateo', 'ip1', 'ip2', 'ip3', 'deet', 'npas', 'datyp', 'nbits', 'grtyp', 'ig1', 'ig2', 'ig3', 'ig4', 'grid', 'd']
@@ -47,6 +48,8 @@ class MinimumHeadersError(Exception):
     pass
 class HeadersAreNotValidError(Exception):
     pass
+class NoHeaderInFileError(Exception):
+    pass
 
 
 class CsvFileReader :   
@@ -82,7 +85,6 @@ class CsvArray:
             self.array= row.d
             a = np.array([[float(j) for j in i.split(',')] for i in array.split(';')],dtype=np.float32, order='F')
             # print("array = " + f'{a}')
-        if(self.hasHeader() and self.checkHeadersAllValid()):
             if(a.ndim == 1):
                 ni = np.shape(a)[0]
                 nj=0
@@ -100,8 +102,6 @@ class CsvArray:
             self.df.at[row.Index,"nj"] = nj
             self.df.at[row.Index,"nk"] = nk
             pass
-        if(self.hasHeader == False):
-            raise NoHeaderError('The csv file you provided does not have headers')
 
     def from_array(self):
         pass
@@ -113,27 +113,109 @@ class CsvArray:
                 open(self.path).read(self.sample_bytes))
 
     def hasMinimumHeaders(self):
-        list_of_hdr_names = self.df.columns.tolist()
-        if 'nomvar' and 'd' and 'level' in list_of_hdr_names:
-            return True
+        if(self.hasHeader):
+            list_of_hdr_names = self.df.columns.tolist()
+            if 'nomvar' and 'd' and 'level' in list_of_hdr_names:
+                return True
+            else:
+                raise MinimumHeadersError('Your csv file doesnt have the necessary columns to proceed! Check that you '
+                                                    + 'have at least nomvar,d and level as columns in your csv file')
         else:
-            raise MinimumHeadersError('Your csv file doesnt have the necessary columns to proceed! Check that you have at least nomvar,d and level as columns in your csv file')
-        
+            raise NoHeaderInFileError('Your file does not have a csv file with headers')
+
     def checkHeadersAllValid(self):
         all_the_cols = BASE_COLUMNS
         all_the_cols.sort()
+
         list_of_hdr_names = self.df.columns.tolist()
         list_of_hdr_names.sort()
+
         set1 = set(list_of_hdr_names)
         set2 = set(BASE_COLUMNS)
-        if(len(list_of_hdr_names) < 19 and self.hasMinimumHeaders):
-            is_subset = set1.issubset(set2)
-            return is_subset
-        if(len(list_of_hdr_names) == 19 and self.hasMinimumHeaders):
-            return all_the_cols == list_of_hdr_names
-        if(len(list_of_hdr_names) > 19):
-            raise HeadersAreNotValidError('The headers in the csv file are not valid. Makes sure that the columns names'
-                                                           + 'are present in BASE_COLUMNS or you have too many columns')
+        if(self.hasMinimumHeaders):
+            if(len(list_of_hdr_names) < 19 and self.hasMinimumHeaders):
+                is_subset = set1.issubset(set2)
+                return is_subset
+
+            if(len(list_of_hdr_names) == 19 and self.hasMinimumHeaders):
+                return all_the_cols == list_of_hdr_names
+
+            if(len(list_of_hdr_names) > 19):
+                raise HeadersAreNotValidError('The headers in the csv file are not valid. Makes sure that the columns names'
+                                                            + 'are present in BASE_COLUMNS or you have too many columns')
+        else:
+            print("Should not print!")
+
+
+
+    def colExists(self,col):
+        if col in self.df.columns:
+            return True
+        else:
+            return False
+
+
+    # DEFAULT VALUES
+    # Probablement va etre ds writer
+    # Remplace toutes les valeurs nbits nulles par 24 en int
+    def checkNbits(self):
+        if(self.colExists("nbits")):
+            self.df.nbits.replace(np.nan,24).apply(np.int64)
+
+    def checkDatyp(self):
+        if(self.colExists("datyp")):
+            self.df.datyp.replace(np.nan,1).apply(np.int64)
+
+    def checkGrTyp(self):
+        if(self.colExists("grtyp")):
+            self.df.grtyp.replace(np.nan,"X")
+
+    def checkDateO(self):
+        if(self.colExists("dateo")):
+            self.df.dateo.replace(np.nan,datetime.utcnow())
+
+    def checkIp1(self):
+        if(self.colExists("ip1")):
+            self.df.ip1.replace(np.nan,3).apply(np.int64)
+
+    def checkIp2EtIp3(self):
+        if(self.colExists("ip2")):
+            self.df.ip2.replace(np.nan,0).apply(np.int64)
+        if(self.colExists("ip3")):
+            self.df.ip3.replace(np.nan,0).apply(np.int64)
+
+    def checkIg(self):
+        if(self.colExists("ig1")):
+            self.df.ig1.replace(np.nan,0).apply(np.int64)
+
+        if(self.colExists("ig2")):
+            self.df.ig2.replace(np.nan,0).apply(np.int64)
+
+        if(self.colExists("ig3")):
+            self.df.ig3.replace(np.nan,0).apply(np.int64)
+
+        if(self.colExists("ig4")):
+            self.df.ig4.replace(np.nan,0).apply(np.int64)
+    
+    def checkEticket(self):
+        if(self.colExists("etiket")):
+            self.df.eticket.replace(np.nan,"CSVREADER")
+
+    
+    
+
+    
+    
+
+
+
+
+
+
+        
+
+
+        
              
 
 
