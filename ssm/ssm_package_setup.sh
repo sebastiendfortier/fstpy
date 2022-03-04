624410269c16f7fe64c225f0b42ecade9e8e9254
Sébastien Fortier <sebastien.fortier@canada.ca>
2022-03-04 17:25:49 +0000

    remove runtime deps

--- ssm/ssm_package_setup.sh
+++ ssm/ssm_package_setup.sh
@@ -1,5 +1,7 @@
 main(){
-      load_runtime_dependencies
+#       load_runtime_dependencies
+   message "Load ci_fstcomp for developpement ..."
+   message '. ssmuse-sh -d /fs/ssm/eccc/cmd/cmds/apps/ci_fstcomp/(check directory for latest version)/'
 }
 
 message(){
@@ -7,19 +9,19 @@ message(){
    true
 }
 
-print_and_do(){
-   message $@
-   eval $@
-}
+# print_and_do(){
+#    message $@
+#    eval $@
+# }
 
-load_runtime_dependencies(){
-   message "Load ci_fstcomp for developpement ..."
-   message '. ssmuse-sh -d /fs/ssm/eccc/cmd/cmds/apps/ci_fstcomp/(check directory for latest version)/'
-   if [[ ! "$PYTHONPATH" == *"rpnpy_2.1.2"* ]]
-   then 
-      print_and_do . r.load.dot eccc/mrd/rpn/MIG/ENV/migdep/5.1.1
-      print_and_do . r.load.dot eccc/mrd/rpn/MIG/ENV/rpnpy/2.1.2
-   fi
-}
+# load_runtime_dependencies(){
+#    message "Load ci_fstcomp for developpement ..."
+#    message '. ssmuse-sh -d /fs/ssm/eccc/cmd/cmds/apps/ci_fstcomp/(check directory for latest version)/'
+#    if [[ ! "$PYTHONPATH" == *"rpnpy_2.1.2"* ]]
+#    then 
+#       print_and_do . r.load.dot eccc/mrd/rpn/MIG/ENV/migdep/5.1.1
+#       print_and_do . r.load.dot eccc/mrd/rpn/MIG/ENV/rpnpy/2.1.2
+#    fi
+# }
 
 main
