94428c28589e9a9da8d1db3ac44013882d1a31e6
sebastiendfortier <76785598+sebastiendfortier@users.noreply.github.com>
2024-09-19 10:22:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
         "Operating System :: OS Linux",
     ],
     install_requires=[
-        'pandas>=1.2.4','numpy>=1.19.5','dask>=2021.8.0','fstd2nc-deps >= 0.20200304.0', 'cmcdict >= 2027.07.22'
+        'pandas>=1.2.4','numpy>=1.23.0','dask>=2021.8.0', 'cmcdict >= 2024.07.22'
     ],
     packages=setuptools.find_packages(exclude='test'),
     include_package_data=True,
